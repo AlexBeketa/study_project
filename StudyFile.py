93cfc9f1a1431345e2d63375cf8d8adc1bfba97f
Alex Beketa <beketa92@gmail.com>
2023-07-04 12:03:31 +0300

    edit studyFile.py

--- StudyFile.py
+++ StudyFile.py
@@ -1,2 +1,6 @@
-print('My study File')
-print('add some changes')
+var_1 = int(input('Введите первое число: '))
+var_2 = int(input('Введите второе число: '))
+
+summ = var_1 + var_2
+
+print(f'Сумма введенных чисел равна: {summ}')
