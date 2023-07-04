ac6468ec3558b54271963b94fc32613538c6452c
Alex Beketa <beketa92@gmail.com>
2023-07-04 21:27:16 +0300

    add lenovo_homepd branch and edit studyFile

--- StudyFile.py
+++ StudyFile.py
@@ -1,8 +1,9 @@
-var_1 = int(input('Введите первое число: '))
+user_var_1 = int(input('Введите первое число: '))
 user_var_2 = int(input('Введите второе число: '))
 user_var_3 = int(input('Введите третье число'))
+user_var_4 = user_var_2 + user_var_3
 
-summ = var_1 + user_var_2
+summ = user_var_1 + user_var_2
 
 
 print(f'Сумма введенных чисел равна: {summ}')
