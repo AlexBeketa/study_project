7a23c00e4eceb5b140b50b585c46074c51a5ea71
AlexBeketa <89851145+AlexBeketa@users.noreply.github.com>
2023-07-04 15:39:41 +0300

    rename var_3, edit summ

--- StudyFile.py
+++ StudyFile.py
@@ -1,7 +1,7 @@
 var_1 = int(input('Введите первое число: '))
-var_2 = int(input('Введите второе число: '))
-var_3 = int(input('Введите третье число'))
+var_2 = float(input('Введите второе число: '))
+user_var_3 = int(input('Введите третье число'))
 
-summ = var_1 + var_2
+summ = var_1 + user_var_2
 
 print(f'Сумма введенных чисел равна: {summ}')
