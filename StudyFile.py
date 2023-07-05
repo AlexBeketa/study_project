a2fa8ab88d324ebc07a425c4594996501f845cff
Alex Beketa <beketa92@gmail.com>
2023-07-05 20:14:10 +0300

    edit user_var_3

--- StudyFile.py
+++ StudyFile.py
@@ -1,6 +1,6 @@
 var_1 = int(input('Введите первое число: '))
 user_var_2 = int(input('Введите второе число: '))
-user_var_3 = int(input('Введите третье число'))
+user_var_3 = int(input(' и это будет противоречить другой ветке'))
 
 summ = var_1 + user_var_2
 
