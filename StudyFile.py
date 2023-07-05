6acdbf2a296dca30f1fb8844748e1156f85d02a9
Alex Beketa <beketa92@gmail.com>
2023-07-05 20:10:20 +0300

    edit func greetings

--- StudyFile.py
+++ StudyFile.py
@@ -2,10 +2,12 @@ var_1 = int(input('Введите первое число: '))
 user_var_2 = int(input('Введите второе число: '))
 user_var_3 = int(input('Введите третье число'))
 
-summ = var_1 + user_var_2
+
 
 def greetings():
-  print('Hello world')
+  summ = var_1 + user_var_2
+  if summ > 10:
+    print('Hello world')
 
 greetings()
-print(f'Сумма введенных чисел равна: {summ}')
+
