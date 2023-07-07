64a5849e754c4a916f939a19cd6c47fefb61b084
Alex Bekta <beketa92@gmail.comfvkexitQgit config --global user.email beketa92@gmail.com>
2023-07-07 19:42:43 +0300

    add amount

--- StudyFile.py
+++ StudyFile.py
@@ -3,9 +3,13 @@ user_var_2 = int(input('Введите второе число: '))
 
 
 def greetings():
+  amount = int(input('Сколько раз вывести сфразу "Привет мир?"'))
   summ = var_1 + user_var_2
   if summ > 10:
-    print('Hello world')
+    for var in range(amount):
+      print('Hello world')
+    
+
 
 greetings()
 
